8c48f0cdf01a77c0ebb1ee985188958a05dcdf5e
SOUNISH NATH <flock.sinasini@gmail.com>
2019-03-18 15:06:57 +0530

    850 refresh controller cookie grabber and CSRF token coded manually for secure hash password

---
+++ music-record-store-back/app/controllers/refresh_controller.rb
@@ -0,0 +1,22 @@
+class RefreshController < ApplicationController
+  before_action :authorize_refresh_by_access_request!
+
+  def create
+    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    tokens  = session.refresh_by_access_allowed do
+      raise JWTSession::Errors::Unauthorized, "Opps..! Something went wrong here!"
+    end
+
+    response.set_cookie(JWTSessions.access_cookie,
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
+
+    render json: { csrf: tokens[:csrf] }
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
---
+++ music-record-store-back/app/controllers/signin_controller.rb
@@ -0,0 +1,33 @@
+class SigninController < ApplicationController
+  before_action :authorize_access_request!, :only => [:destroy]
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.authenticate(params[:password])
+      payload = { user_id: user.id }
+      session = JWTSession::Session.new(payload: payload, refresh_by_access_allowed: true )
+      tokens = session.login
+      response_set_cookie(JWTSessions.access_cookie,
+                          value: tokens[:access],
+                          httponly: true,
+                          secure: Rails.env.production?)
+      render json: { csrf: tokens[:csrf] }
+    else
+      not_found
+    end
+  end
+
+  def destroy
+    session = JWTSessions::Session.new(payload: payload)
+    session.flush_by_by_access_payload
+    render json: :ok
+  end
+
+
+  private
+
+    def not_found
+      render json: { error: "Cannot find email/password combination" }, status: :not_found
+    end
+end
