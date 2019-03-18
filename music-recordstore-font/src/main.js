3079be854289155c5178d24034aa666f10b39f31
SOUNISH NATH <flock.sinasini@gmail.com>
2019-03-18 17:11:14 +0530

    axios hard to import ERR

--- music-recordstore-font/src/main.js
+++ music-recordstore-font/src/main.js
@@ -3,10 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import VueAxios from 'vue-axios' //why couldn't started
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
 import './main.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 /* eslint-disable no-new */
 new Vue({
